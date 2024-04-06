2eb8ba84c97ae87df7afc1a60c5ac9b24c9a4bf5
kunalkendrekar115 <kunalkendrekar115@gmail.com>
2024-04-06 17:53:28 +0530

    changed package2

--- packages/package2/lib/index.js
+++ packages/package2/lib/index.js
@@ -3,7 +3,7 @@
 const { calculateSum } = require("package1");
 function package2() {
   const sum = calculateSum([1, 2, 3]);
-  console.log(sum);
+  console.log("Sum is ", sum);
 }
 
 console.log("Hello from package2");
